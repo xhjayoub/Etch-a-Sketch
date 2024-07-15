b78d572e8becfc7168923bf6f48d68872d9774ee
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-15 13:51:23 +0100

    Change square background color on hover

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ const container = document.querySelector(".container");
 for (let i = 0; i<256 ; i++) {
     container.appendChild(CreateDiv())
 }
+
+const arrDivs = document.querySelectorAll(".square");
+arrDivs.forEach((e) => {
+    e.addEventListener('mouseover',() => {
+        e.style.backgroundColor = "blue";
+    })
+})
