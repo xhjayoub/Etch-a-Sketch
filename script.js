b34733790ed267d4a7bea4b568aa54a263640e36
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-15 14:25:03 +0100

    Add action to generate and reset buttons

--- script.js
+++ script.js
@@ -5,15 +5,38 @@ function CreateDiv() {
 }
 
 
-const container = document.querySelector(".container");
 
-for (let i = 0; i<256 ; i++) {
-    container.appendChild(CreateDiv())
-}
-
-const arrDivs = document.querySelectorAll(".square");
-arrDivs.forEach((e) => {
-    e.addEventListener('mouseover',() => {
+function generateSquares(n) {
+    const container = document.querySelector(".container");
+    for (let i = 0; i<(n*n) ; i++) {
+        container.appendChild(CreateDiv())
+    }
+    const arrDivs = document.querySelectorAll(".square");
+    arrDivs.forEach((e) => {
+        e.addEventListener('mouseover',() => {
         e.style.backgroundColor = "blue";
     })
+})
+}
+
+function getSquaresNum() {
+    return parseInt(document.querySelector("#squaresNum").value);
+}
+
+
+
+
+generateSquares(16);
+
+const generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click",() => {
+    document.querySelector(".container").innerHTML = "";
+    generateSquares(getSquaresNum());
+})
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener("click", () => {
+    document.querySelector(".container").innerHTML = "";
+    generateSquares(16);
+    document.querySelector("#squaresNum").value = 16;
 })
