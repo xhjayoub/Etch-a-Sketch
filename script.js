c034787328367db250c1f71998aab909bb668efe
Ayoub hj <medayoubhajamor197@gmail.com>
2024-07-16 12:25:49 +0100

    Make sketch look like a grid of squares
    With any number

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-function createSquare() {
+function createSquare(n, width, height) {
+    console.log(width, height)
     const div = document.createElement("div");
     div.setAttribute("class","square");
+    console.log(`height: ${height / n}px; width: ${width / n}px;`);
+    div.style.cssText = `height: ${height / n}px; width: ${width / n}px;`;
     return div;
 }
 
-function createRow(n) {
+function createRow(n,containerWidth, containerHeight) {
+    console.log(containerHeight, containerWidth)
     const div = document.createElement("div");
     div.setAttribute("class","row");
     for (let i = 0; i<n; i++) {
-        div.appendChild(createSquare());
+        div.appendChild(createSquare(n,containerWidth, containerHeight));
     }
     return div;
 }
 
 function generateSketch(n) {
     const container = document.querySelector(".container");
+    let containerWidth = container.offsetWidth;
+    let containerHeight = container.offsetHeight;
     for (let i = 0; i<n; i++) {
-        container.appendChild(createRow(n))
+        container.appendChild(createRow(n,containerWidth, containerHeight))
     }
 }
+generateSketch(10);
